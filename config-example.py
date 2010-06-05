91a0312a8e036c1529425f6ca840ee6cf0913f92
Ian Mulvany <ian@pbl-ian.home>
2010-06-05 16:06:47 +0100

    sample config file

---
+++ config-example.py
@@ -0,0 +1,11 @@
+# your flickr api key
+apikey = ''
+
+# the flickr id of the group you are interestd in
+GROUPID = "" 
+
+# the max limit for calls to the appengine datastore
+FETCHLIMIT = 1000
+
+# number of items to be displayed on pages that page items
+PAGINGLIMIT = 10
